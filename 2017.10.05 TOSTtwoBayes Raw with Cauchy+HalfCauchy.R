1cdeddf1c29346fcdbb35f7e0c439ee903b0f7cd
shaggy mac <shaggymac@dyn-10-32-110-127.winet.local>
2017-11-22 15:28:42 +0000

    removed abs()
    * BF now sensitive to direction of results

--- 2017.10.05 TOSTtwoBayes Raw with Cauchy+HalfCauchy.R
+++ 2017.10.05 TOSTtwoBayes Raw with Cauchy+HalfCauchy.R
@@ -114,7 +114,7 @@ TOSTtwo.raw.nm<-function(m1,m2,sd1,sd2,n1,n2,low_eqbound, high_eqbound, alpha, v
     dist_theta[theta <= 0] = 0
   }
   dist_theta_alt = dist_theta/sum(dist_theta)
-  likelihood <- dt((abs(dif)-theta)/(abs(dif)/abs(t)), df = degree_f) #use abs(dif) - can be set to d
+  likelihood <- dt((dif-theta)/(dif/t), df = degree_f) #use dif - can be set to d
   likelihood_alt = likelihood/sum(likelihood)
   height <- dist_theta * likelihood
   area <- sum(height * incr)
@@ -122,7 +122,7 @@ TOSTtwo.raw.nm<-function(m1,m2,sd1,sd2,n1,n2,low_eqbound, high_eqbound, alpha, v
   height_alt = dist_theta_alt * likelihood_alt
   height_alt = height_alt/sum(height_alt)
   LikelihoodTheory <- area/normarea
-  LikelihoodNull <- dt(abs(dif)/(abs(dif)/abs(t)), df = degree_f)
+  LikelihoodNull <- dt(dif/(dif/t), df = degree_f)
   BayesFactor <- round(LikelihoodTheory / LikelihoodNull, 2)
   bayes_results <- data.frame(BayesFactor, LikelihoodTheory, LikelihoodNull)
   colnames(bayes_results) <- c("Bayes Factor","Likelihood (alternative)","Likelihood (null)")
@@ -130,7 +130,6 @@ TOSTtwo.raw.nm<-function(m1,m2,sd1,sd2,n1,n2,low_eqbound, high_eqbound, alpha, v
   print(bayes_results)
   cat("\n")
   invisible(list(TOST_t1=t1,TOST_p1=p1,TOST_t2=t2,TOST_p2=p2, TOST_df=degree_f,alpha=alpha,low_eqbound=low_eqbound,high_eqbound=high_eqbound,low_eqbound=low_eqbound,high_eqbound=high_eqbound, LL_CI_TOST=LL90,UL_CI_TOST=UL90,bf=BayesFactor, ll_theory=LikelihoodTheory, ll_null=LikelihoodNull))
-  
   
   #plot (adapted from Wienes by DL)
   myminY = 1
@@ -168,69 +167,38 @@ TOSTtwo.raw.nm<-function(m1,m2,sd1,sd2,n1,n2,low_eqbound, high_eqbound, alpha, v
   }
 }
   
-
-library("TOSTER")
-##### Correct Understanding of TOST?
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "halfnormal", effect_prior = 0.5) # B = 0.31
-# Nonsignificantly different from zero t=0.23, p=0.82, and is equivalent t=3.50, p=.0002.
-# B = 0.3
-
-TOSTtwo.raw.nm(m1=5.25,m2=4.75,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "halfnormal", effect_prior = 0.5) # B = 0.31
-# Significantly different from zero t=3.81, p<.001, and is not equivalent t=0.53, p=.70
-# B = 354.6
-
-TOSTtwo.raw.nm(m1=5.25,m2=5.05,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "halfnormal", effect_prior = 0.5) # B = 0.31
-# Not significantly different from zero t=1.52, p<.001, and was equivalent t=-1.75, p=.04
-# B = 1.4
-
-
-
-
-
-# Calculate SE for original study
-# In the future, could just run the TOST function and divide Mdiff by t.
-(sqrt((((20 - 1)*(0.59^2)) + (20 - 1)*(0.62^2))/((20+20)-2))) * sqrt(1/20 + 1/20) # 0.1913766, SE for original study
-(sqrt((((95 - 1)*(0.95^2)) + (89 - 1)*(0.83^2))/((95+89)-2))) * sqrt(1/95 + 1/89) # 0.131882, SE for replication study
+## Checking directionality - Moon and Roeder data
+# means one way
+TOSTtwo.raw.nm(m1  = 0.50,
+        m2  = 0.46,
+        sd1 = 0.18,
+        sd2 = 0.17,
+        n1  = 48,
+        n2  = 53,
+        low_eqbound  = -0.0625,
+        high_eqbound =  0.0625,
+        alpha = .05,
+        prior_dist="halfnormal",
+        effect_prior=0,
+        se_prior=0.05
+)
+# B = 1.47
 
 
-## Normals
-# TOST raw calculator original
-TOSTtwo.raw(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "halfnormal", effect_prior = 0.5) # B = 0.31
-TOSTtwo.raw(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "normal", effect_prior = 0.5) # B = 0.12
-# Baguely & Kaye's calculator:
-Bf(0.132, 0.03, 0, meanoftheory=0, sdtheory=0.5, tail=1) # 0.30   
-Bf(0.132, 0.03, 0, meanoftheory=0.5, sdtheory=0.25, tail=2) # 0.12
-# Wiens calculator:
-BF_t(0, 0.5, 100000, 0.03, 0.132, (89+95)-2, tail=1) # 0.31
-BF_t(0.5, 0.25, 100000, 0.03, 0.132, (89+95)-2, tail=2) # 0.12
 
 
-## t-distributions
-# TOST raw calculator original
-TOSTtwo.raw(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.433, var.equal=FALSE, prior_dist = "halfnormal", effect_prior = 0, se_prior=0.5, df_prior=(20+20)-2) # B = 0.30
-TOSTtwo.raw(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "normal", effect_prior = 0.5, se_prior=0.191, df_prior=(20+20)-2) # B = 0.08
-# Dienes & McLatchie calculator:
-Bft(0.132, 0.03, (95+89)-2, meanoftheory=0, sdtheory=0.50, dftheory=(20+20)-2, tail = 1) # 0.3063493
-Bft(0.132, 0.03, (95+89)-2, meanoftheory=0.50, sdtheory=0.191, dftheory=(20+20)-2, tail = 2) # 0.07860583
-# Wiens calculator:
-BF_t(0, 0.5, (20+20)-2, 0.03, 0.132, (89+95)-2, tail=1) # 0.30
-BF_t(0.5, 0.191, 100000, 0.03, 0.132, (89+95)-2, tail=2) # 0.08
-
-
-## Cauchy
-# TOST raw calculator original
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "halfnormal", effect_prior = 0, se_prior=0.5, df_prior=1) # B = 0.25
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "normal", effect_prior = 0.5, se_prior=0.25, df_prior=1) # B = 0.12, using se_prior = 0.5/2
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "normal", effect_prior = 0.5, se_prior=0.191, df_prior=1) # B = 0.10, using actual se_prior
-# TOST raw calculator neil
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "halfcauchy", effect_prior = 0.5) # B = 0.25
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "cauchy", effect_prior = 0.5) # B = 0.12, using se_prior = 0.5/2 (default)
-TOSTtwo.raw.nm(m1=5.25,m2=5.22,sd1=0.95,sd2=0.83,n1=95,n2=89,low_eqbound=-0.43,high_eqbound=0.43, var.equal=FALSE, prior_dist = "cauchy", effect_prior = 0.5, se_prior=0.191) # B = 0.10, using actual se_prior
-# Dienes & McLatchie calculator:
-Bft(0.132, 0.03, (95+89)-2, meanoftheory=0, sdtheory=0.50, dftheory=1, tail = 1) # 0.2549421
-Bft(0.132, 0.03, (95+89)-2, meanoftheory=0.50, sdtheory=0.25, dftheory=1, tail = 2) # 0.1029683
-# Wiens calculator:
-BF_t(0, 0.5, 1, 0.03, 0.132, (95+89)-2, tail=1) # 0.25
-BF_t(0.5, 0.191, 1, 0.03, 0.132, (95+89)-2, tail=2) # 0.10
-
-
+# means switched around
+TOSTtwo.raw.nm(m1  = 0.46,
+        m2  = 0.50,
+        sd1 = 0.17,
+        sd2 = 0.18,
+        n1  = 53,
+        n2  = 48,
+        low_eqbound  = -0.0625,
+        high_eqbound =  0.0625,
+        alpha = .05,
+        prior_dist="halfnormal",
+        effect_prior=0,
+        se_prior=0.05
+)
+# B = 0.31
