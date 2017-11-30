b5cf83ca3ceaaa83828dc07460d97224b10657c2
Daniel Lakens <Lakens@gmail.com>
2017-11-30 08:10:13 +0100

    source functions

--- ERROR CHECK TOSTr.bf.R
+++ ERROR CHECK TOSTr.bf.R
@@ -6,8 +6,14 @@
 # Baguley & Kaye:      http://danny-kaye.co.uk/Docs/Dienes_functions.txt
 # Wienes:              https://figshare.com/authors/Stefan_Wiens/3862558
 
+#Load functions
 
-
+source('TOSTtwo.raw.bf.R') #Load function for raw mean differences
+source('TOSTtwo.bf.R') #Load function for standardized mean differences
+source('TOSTr.bf.R') #Load function for standardized mean differences
+source('Leftovers/bf_Baguley.R') #Load function by Baguley Kaye: http://danny-kaye.co.uk/Docs/Dienes_functions.txt
+source('Wiens_functions/BF_t.R') #Load function by Wiens
+source('Wiens_functions/BF_U.R') #Load function by Wiens
 
 ## This function transforms r to Fisher's z and calculates its SE
 ## Required for Wienes and Baguley&Kaye
