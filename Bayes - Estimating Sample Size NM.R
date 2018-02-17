ca8092fe49f5f19f82abf9283f5fc7a9af5c4a55
Neil <n.mclatchie@lancaster.ac.uk>
2018-02-17 18:18:19 +0000

    Estimating sample size

---
+++ Bayes - Estimating Sample Size NM.R
@@ -0,0 +1,64 @@
+# Estimating sample size
+# Instructions from: https://www.youtube.com/watch?v=10Lsm_o_GRg
+
+source('DienesMcLatchie.R')
+
+# Example data from Gerontology Article:
+# Bunce, Batterham & Mackinnon (2018)
+# Found non-frail adults 
+
+# Need: SE from a previous study 
+bunce.se <- (11.53-10.11) / 4.20 # 0.3380952
+
+# Need: N from previous study 
+bunce.n <- 304 + 154 # 458
+
+#Predicted effect size for H1 
+bunce.mdif <- 11.53-10.11 # 1.42
+obtain.h1 <- bunce.mdif
+
+# Predicted effect size for H0 (here = 0)
+obtain.h0 <- bunce.mdif
+
+
+## Sample size estimation
+
+# Number of subjects we are willing to test
+n = seq(10, 250, 1)
+
+# standard error of the estimate based on the obtained SD
+se_diff = (bunce.se)*sqrt(bunce.n/n) # the standard error varies as the square root of n, or in this case, the change in the magnitude of n
+
+# Calculate B for different sample sizes for H0
+x <- 1/Bft(se_diff , 0  , n-1, meanoftheory=0, sdtheory=1.42, dftheory=100000, tail=1)
+x # 207 participants = 3.005284
+
+# Calculate B for different sample sizes for H1
+y <- Bft(se_diff , 1.42  , n-1, meanoftheory=0, sdtheory=1.42, dftheory=100000, tail=1)
+y # 82 participants = 3.025246
+
+
+
+# plot H1 uniform
+
+plot(x = x,
+     type = "l",                            # Specify that you want to plot a line graph
+     lwd = 2,                               # Thickness of line
+     lty = 2,
+     col = "black",                          # Colour of line
+     xlim=c(0,200),                          # Set limit of x-axis
+     ylim=c(0,10),                         # Set limit of y-axis
+     frame.plot=TRUE,                       # Do plot the frame of the graph
+     xlab="Sample Size",                    # Title for x-axis
+     ylab= "Bayes Factor",                             # Title for y-axis
+     axes = FALSE,                          # Don't plot the scales by default
+     main=paste("Estimating Sample Size for Bayes Factors"))     # Main title
+segments(0, 3, 250, 3, col= 'black', lwd=2, lty=1)          # Add a black horizontal line
+lines(y[y<10 ], lwd=2, lty=3)
+Axis(side=1,at=seq(0, 200, by = 10))
+Axis(side=2,at=seq(0,10, by=1))
+legend(0,10,legend=c("B for H0 (Mdiff = 0ms)","B for H1 (Mdiff = 25ms)", "Threshold for sensitive test (B=3)"),
+       text.col="black", lty = c(2,3,1))
+
+dev.copy(png,'Estimating Sample Size Based on Bunce et al 2018.png')             # Save whatever is in displayed in the plot viewer in my wd
+dev.off()
