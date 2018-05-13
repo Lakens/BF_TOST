43b5c082b70d7134a45cf912dd69d9a1835135b8
Neil McLatchie <neilmclatchie@fyd008a00003.lancs.ac.uk>
2018-05-13 19:42:53 +0100

    Realised BFs calculated incorrectly. Options:
    1. Caluculate using standardised coefficients
    2. Calculatte using raw coefficients (but if we use TOSTER function to do this, it will calculate incorrect Equivalence Tests).

--- Example_3_Westerhof_corr.R
+++ Example_3_Westerhof_corr.R
@@ -1,12 +1,65 @@
 # Example 3. Westerhof, Bohlmeijer & McAdams (2015) predicts that Big Five NEO-FFI openness should be related to Ego integrity. 
 # The authors conclude "higher levels of openness to experience are significantly related to more ego integrity but not to despair." after finding a significant .14 correlation between openness and ego integrity, and a non-significant .12 correlation between openness and despair.
+source('TOSTtwo.raw.bf.R') #Load function for raw mean differences
 source("TOSTr.bf.R")
+
 n <- 218
 r <- 0.12
 sesoi <- 0.14  # Since the authors treat this as an interesting correlation, and treat a correlation of .12 as small enough to consider 0, we can assume that .14 is quite close to their smalles effect size of interest (which is of course wrong, as they later judge another .12 correlation to be interesting when it is significant)
 
+
+
+
+
+
+## Using the beta coefficients
+## r x (SD Despair)/(SD Openness)
+b.prior = 0.14 * 0.8/0.6 # 0.19
+b.obtained <- 0.12 * 1.0/0.6 # 0.20 
+
+
+
+# What we did previously (and incorrectly)
+# Used r (and subsequent fishers z) to model the data and b to specify the prior.
+TOSTr.bf(n = 218, 
+         r = 0.12, 
+         low_eqbound_r = -sesoi, 
+         high_eqbound_r = sesoi, 
+         prior_dist = "halfnormal", 
+         effect_prior = 0, 
+         se_prior = 0.19,  
+         df_prior = 10000)
+# B = 2.56111
+
+# Option A: Specify using standardised effect r (and the calculator while transform both using fisher's z)
+TOSTr.bf(n = 218, 
+         r = 0.12, 
+         low_eqbound_r = -sesoi, 
+         high_eqbound_r = sesoi, 
+         prior_dist = "halfnormal", 
+         effect_prior = 0, 
+         se_prior = 0.14,  
+         df_prior = 10000)
+# B = 2.915786
+
+# Option B: Specify using raw effects (will still be transformed, but won't make a difference)
+# Need to check SEM of beta coefficient is being calculated correctly.
+# This also doesn't work for the equivalence tests, that need to specify standardised r
+TOSTr.bf(n = n, 
+         r = .20, 
+         low_eqbound_r = -sesoi, 
+         high_eqbound_r = sesoi, 
+         prior_dist = "halfnormal", 
+         effect_prior = 0, 
+         se_prior = .19,  
+         df_prior = 10000)
+# B = 31.63302
+
+
+
+
 # BF = based on authors interpreting 0.14 as meaningful
-# raw slope = 0.14 * 0.8/0.6 = 0.19
+
 TOSTr.bf(n = n, 
          r = r, 
          low_eqbound_r = -sesoi, 
@@ -43,8 +96,7 @@ TOSTr.bf(n = n,
 
 
 # Converting standardised to raw units
-# r x (SD Despair)/(SD Openness)
-b.obtained <- 0.12 * 1.0/0.6 # 0.20 
+
 
 # Raw SE
 # 1/sqrt(N-3)
@@ -67,3 +119,4 @@ TOSTr.bf(n = n,
          se_prior = 0.40,  
          df_prior = 10000)
 # B = 1.468118
+
